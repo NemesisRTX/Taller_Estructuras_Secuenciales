72fe957527faaa7c7cbcac2eee248041d6dcadad
Nicolas Velez <nvelezc71770@universidadean.edu.co>
2022-08-08 01:29:19 -0400

    Taller Estructuras Secuenciales
    Solución taller estructuras secuenciales.

--- Ejercicio_10.py
+++ Ejercicio_10.py
@@ -1,5 +1,14 @@
 #Entradas
-
+nume1=float(input("Ingrese cantidad de chelines austriacos para convertir a pesetas: "))
+nume2=float(input("Ingrese cantidad de dracmas griegos para convertir a francos franceses: "))
+nume3=float(input("Ingrese cantidad de pesetas para convertir a dólares y liras italianas: "))
 #Caja Negra
-
+pesetas=((nume1*956.871)/100)
+francos=((nume2*88.607)/(100*20.110))
+dolares=(nume3/122.499)
+liras=((nume3*100)/9.289)
 #Salidas
+print(f"${nume1} chelines austriacos equivalen a: ${pesetas} pesetas.")
+print(f"${nume2} dracmas griegos equivalen a: ${francos} francos.")
+print(f"${nume3} pesetas equivalen a: ${dolares} dolares.")
+print(f"${nume3} pesetas equivalen a: ${liras} liras.")
--- Ejercicio_11.py
+++ Ejercicio_11.py
@@ -1,5 +1,24 @@
 #Entradas
-
+nombre=str(input("Ingrese nombre del trabajador: "))
+hijos=int(input("Ingrese numero de hijos del trabajador: "))
+hogares=int(input("Ingrese numero de hogares del trabajador: "))
+act=int(input("Ingrese la cantidad de actualizaciones academicas del trabajador: "))
+nhoras=int(input("Ingrese numero de horas de trabajo: "))
+phoras=float(input("Ingrese el precio por una hora: "))
+ehoras=int(input("Ingrese numero de horas extra: "))
 #Caja Negra
-
+pehoras=(ehoras*0.25)
+sbase=(nhoras*phoras)
+d1=(sbase*0.95)
+d2=(sbase*0.98)
+d3=(sbase*0.93)
+dt=(d1+d2+d3)
+a1=(sbase+250000*act)
+a2=(sbase+173000*hijos)
+a3=(sbase+180000*hogares)
+at=(a1+a2+a3)
+sneto=(sbase+pehoras+dt+at)
 #Salidas
+print(f"Las deducciones sobre el sueldo son de ${dt}.")
+print(f"Las asignaciones sobre el sueldo son de ${at}.")
+print(f"El sueldo neto de {nombre} es: ${sneto}.")
